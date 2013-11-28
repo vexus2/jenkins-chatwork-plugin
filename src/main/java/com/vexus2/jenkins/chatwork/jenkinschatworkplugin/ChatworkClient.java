4c00a7b844c13c68936cf566878e24c84a14b5f6
hikaru.tooyama <hikaru.tooyama@nanapi.co.jp>
2013-11-28 19:03:18 +0900

    initial commit.

---
+++ src/main/java/com/vexus2/jenkins/chatwork/jenkinschatworkplugin/ChatworkClient.java
@@ -0,0 +1,117 @@
+package com.vexus2.jenkins.chatwork.jenkinschatworkplugin;
+
+import hudson.model.AbstractBuild;
+import hudson.model.CauseAction;
+import hudson.scm.ChangeLogSet;
+import org.apache.commons.lang.StringUtils;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class ChatworkClient {
+
+  public final String API_KEY;
+
+  public final String CHANNEL_ID;
+
+  public final AbstractBuild BUILD;
+
+  public static final String API_URL = "https://api.chatwork.com/v1";
+
+
+  public ChatworkClient(AbstractBuild build, String apiKey, String channelId) {
+    this.BUILD = build;
+    this.API_KEY = apiKey;
+    this.CHANNEL_ID = channelId;
+  }
+
+  public boolean sendMessage() throws Exception {
+    if (this.BUILD == null || this.API_KEY == null || this.CHANNEL_ID == null) {
+      throw new Exception("API Key or Channel ID is null");
+    }
+
+    String message = createMessage();
+
+    String url = new StringBuffer(API_URL).append("/").append(this.CHANNEL_ID).append("/messages").toString();
+    URL obj = new URL(url);
+    HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+    con.setRequestMethod("POST");
+    con.setRequestProperty("X-ChatWorkToken", this.API_KEY);
+
+    String urlParameters = "data=" + message;
+
+    con.setDoOutput(true);
+    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+    wr.writeBytes(urlParameters);
+    wr.flush();
+    wr.close();
+
+    int responseCode = con.getResponseCode();
+    if(responseCode != 200) {
+      throw new Exception("Response is not valid. Check your API Key or Chatwork API status. response_code = " + responseCode);
+    }
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader in = new BufferedReader(
+        new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
+    }
+    in.close();
+
+    return true;
+  }
+
+  private String createMessage() throws Exception {
+    StringBuffer message = new StringBuffer();
+    String changes = getChanges();
+    CauseAction cause = this.BUILD.getAction(CauseAction.class);
+
+    if (changes != null) {
+      message.append(changes + "¥n");
+    } else if (cause != null) {
+      message.append(cause + "¥n");
+    }
+
+    return message.toString();
+  }
+
+  String getChanges() throws Exception {
+    if (!this.BUILD.hasChangeSetComputed()) {
+      throw new Exception("No change set computed...");
+    }
+    ChangeLogSet changeSet = this.BUILD.getChangeSet();
+    List<ChangeLogSet.Entry> entries = new LinkedList<ChangeLogSet.Entry>();
+    Set<ChangeLogSet.AffectedFile> files = new HashSet<ChangeLogSet.AffectedFile>();
+    for (Object o : changeSet.getItems()) {
+      ChangeLogSet.Entry entry = (ChangeLogSet.Entry) o;
+      entries.add(entry);
+      files.addAll(entry.getAffectedFiles());
+    }
+    Set<String> authors = new HashSet<String>();
+    if (!entries.isEmpty()) {
+      for (ChangeLogSet.Entry entry : entries) {
+        authors.add(entry.getAuthor().getDisplayName());
+      }
+    }
+
+    StringBuffer message = new StringBuffer();
+    message.append("Started by changes from ");
+    message.append(StringUtils.join(authors, ", "));
+    message.append(" (");
+    message.append(files.size());
+    message.append(" file(s) changed)");
+    return message.toString();
+  }
+}
---
+++ src/main/java/com/vexus2/jenkins/chatwork/jenkinschatworkplugin/ChatworkPublisher.java
@@ -0,0 +1,93 @@
+package com.vexus2.jenkins.chatwork.jenkinschatworkplugin;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+public class ChatworkPublisher extends Publisher {
+
+  private final String rid;
+
+  // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+  @DataBoundConstructor
+  public ChatworkPublisher(String rid) {
+    this.rid = rid;
+  }
+
+  /**
+   * We'll use this from the <tt>config.jelly</tt>.
+   */
+  public String getRid() {
+    return rid;
+  }
+
+  @Override
+  public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+
+    Boolean result = true;
+    try {
+      ChatworkClient chatworkClient = new ChatworkClient(build, getDescriptor().getApikey(), getRid());
+      chatworkClient.sendMessage();
+    }catch (Exception e) {
+      result = false;
+      listener.getLogger().println(e.getMessage());
+    }
+    return result;
+  }
+
+  @Override
+  public DescriptorImpl getDescriptor() {
+    return (DescriptorImpl) super.getDescriptor();
+  }
+
+  public BuildStepMonitor getRequiredMonitorService() {
+    return BuildStepMonitor.BUILD;
+  }
+
+  /**
+   * Descriptor for {@link ChatworkPublisher}. Used as a singleton.
+   * The class is marked as public so that it can be accessed from views.
+   * <p/>
+   * <p/>
+   * See <tt>src/main/resource/com.vexus2.jenkins.chatwork.jenkinschatworkplugin/ChatworkPublisher/*.jelly</tt>
+   * for the actual HTML fragment for the configuration screen.
+   */
+  @Extension
+  // This indicates to Jenkins that this is an implementation of an extension point.
+  public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+
+    private String apikey;
+
+    public String getApikey() {
+      return apikey;
+    }
+
+    public DescriptorImpl() {
+      load();
+    }
+
+    public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+      return true;
+    }
+
+    public String getDisplayName() {
+      return "Jenkins Chatwork Plugin";
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+      apikey = formData.getString("apikey");
+      save();
+      return super.configure(req, formData);
+    }
+  }
+}
+
